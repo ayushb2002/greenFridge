4f75409a5e92a33c4013ea8c2fd7e51889396afd
Abhiram B.S.N <abhirambsn@gmail.com>
2022-05-15 21:22:58 +0530

    Added Camera Permissions

--- frontend/screens/home.js
+++ frontend/screens/home.js
@@ -1,79 +1,87 @@
-import React from 'react';
-import { Button, StyleSheet, View, Text,TouchableOpacity } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React from "react";
+import { Button, StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Camera } from "expo-camera";
 
-export default function Home(){
-    return(
-      <View style={styles.bg}>
-
-        <View style={styles.frontPageBut}>
-
+export default function Home() {
+  return (
+    <View style={styles.bg}>
+      <Camera>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.but}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      <View style={styles.frontPageBut}>
         <SafeAreaView style={styles.alternativeLayoutButtonContainer}>
-         <TouchableOpacity style={styles.but}>
-         <Text style={styles.butText}>MENU</Text>
-         </TouchableOpacity>
+          <TouchableOpacity style={styles.but}>
+            <Text style={styles.butText}>MENU</Text>
+          </TouchableOpacity>
 
-         <TouchableOpacity style={styles.but2}>
-         <Text style={styles.butText}>SNAP</Text>
-         </TouchableOpacity>
+          <TouchableOpacity style={styles.but2}>
+            <Text style={styles.butText}>SNAP</Text>
+          </TouchableOpacity>
 
-         <TouchableOpacity style={styles.but}>
-         <Text style={styles.butText}>GALLERY</Text>
-         </TouchableOpacity>
+          <TouchableOpacity style={styles.but}>
+            <Text style={styles.butText}>GALLERY</Text>
+          </TouchableOpacity>
         </SafeAreaView>
-        </View>
-
-
-        </View>
-    );
+      </View>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-     flex: 1,
-     justifyContent: 'center',
-    },
-    buttonContainer: {
-      margin: 20
-    },
-    alternativeLayoutButtonContainer: {
-      
-      width:"90%",
-      aligSelf:'flex-end',
-      top: 0,
-      margin: 20,
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    },
-    but: {
-      borderRadius: 10,
-      backgroundColor: "#7CB342",
-      alignItems: "center",
-      height: 40,
-      width: 60,
-      justifyContent: "center",
-    },
-    butText: {
-      color: "white",
-      fontWeight: "bold",
-    },
-    but2: {
-      borderRadius: 50,
-      backgroundColor: "#7CB342",
-      alignItems: "center",
-      padding: 10,
-      justifyContent: "center",
-    },
-    bg: {
-      backgroundColor:"black"
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    margin: 20,
+  },
+  alternativeLayoutButtonContainer: {
+    width: "90%",
+    aligSelf: "flex-end",
+    top: 0,
+    margin: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  but: {
+    borderRadius: 10,
+    backgroundColor: "#7CB342",
+    alignItems: "center",
+    height: 40,
+    width: 60,
+    justifyContent: "center",
+  },
+  butText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  but2: {
+    borderRadius: 50,
+    backgroundColor: "#7CB342",
+    alignItems: "center",
+    padding: 10,
+    justifyContent: "center",
+  },
+  bg: {
+    backgroundColor: "black",
+  },
 
-    frontPageBut: {
-      backgroundColor: "pink"
-    }
-  
-  });
-  
-  
+  frontPageBut: {
+    backgroundColor: "pink",
+  },
+});
